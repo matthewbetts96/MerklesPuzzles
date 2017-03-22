cc1132aa0cd763bf4b0970dde2ec0b1162e43020
mbetts <matthew.betts.96@gmail.com>
2017-03-22 00:28:12 +0000

    Most of the work for decryption is there, just doesn't quite work yet...

--- Merkle.java
+++ Merkle.java
@@ -13,19 +13,110 @@ import javax.crypto.spec.DESKeySpec;
 import java.util.*;
 import java.io.*;
 import java.security.SecureRandom;
+import java.util.Random;
+	/*		keep this for now, will be useful later
+			System.out.println("concatarray = " + byteArrayToString(c));
+			//These return the original inputs by splitting the byte array 
+			byte[] arr2 = Arrays.copyOfRange(c, 18, 26);
+	   		System.out.println("DESkey (after copy) = " + byteArrayToString(arr2));
 
-public class Test {
+			byte[] arr3 = Arrays.copyOfRange(c, 0, 15);
+	   		System.out.println("puzzlestart (after copy) = " + byteArrayToString(arr3));*/
+
+public class Merkle {
+
 	static Cipher cipher;
-	
+
 	public static void main (String[] args){
-		int n =1;
+		try { 
+			new File("puzzles.txt").delete();
+		} catch(Exception e) { System.out.print("Error. It's possible the file doesn't exist yet."); }
+		
+		int n = 1;
 		while(n <= 1024){
 			createPuzzles(n);
 			n++;
 		}
+
+		boolean solved = false;
+		int attempts = 0;
+		
+		String puzzleToCrack = selectPuzzle();
+		System.out.println("Puzzle to crack: " + puzzleToCrack);
+		byte[] ciphertext = stringToByteArray(puzzleToCrack);
+		System.out.print("Key cracking beginning..");
+		
+		byte[] decrypted = null;
+		while(!solved){
+			attempts++;
+			try {
+				decrypted = decrypt(ciphertext, attempts);
+				if(decrypted != null){
+					solved = true;
+				}
+			} catch (Exception e){ System.err.println("Caught Exception: " + e.getMessage()); }
+
+		}
+		System.out.println("Key Cracked. " + attempts + " attempts.");
+		System.out.println(byteArrayToString(decrypted));
+
+		byte[] arr1 = Arrays.copyOfRange(decrypted, 0, 25);
+	   	System.out.println("Should be all A's = " + byteArrayToString(arr1));
+		byte[] arr2 = Arrays.copyOfRange(decrypted, 16, 18);
+	   	System.out.println("2 digit num = " + byteArrayToSmallInt(arr2));
+		byte[] arr3 = Arrays.copyOfRange(decrypted, 19, 25);
+	   	System.out.println("random key = " + byteArrayToString(arr3));
+	}
+
+	public static byte[] decrypt(byte[] ciphertext, int attempts) throws Exception {
+		cipher = Cipher.getInstance("DES");
+		byte[] combinedKey = new byte[8];
+		byte[] possibleKeyEnd = new byte[6];
+		byte[] bytedAttempts = (smallIntToByteArray(attempts));
+		try {
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			outputStream.write(bytedAttempts); //2 bytes
+			outputStream.write(possibleKeyEnd); //6 bytes
+			combinedKey = outputStream.toByteArray();
+		} catch (Exception e) { System.err.println("Caught Exception: " + e.getMessage()); }
+
+		SecretKey key = generateDESKey(combinedKey);
+		try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decrypted = cipher.doFinal(ciphertext);
+            return decrypted;
+        } catch (Exception e) {
+        	//System.err.println("Caught Exception: " + e.getMessage());
+        }
+        return null;
+	}
+
+
+	//Collects all puzzles and returns the string of the chosen puzzle
+	public static String selectPuzzle(){
+		System.out.println("Carefully selecting a puzzle...");
+		ArrayList<String> puzzleList = new ArrayList<String>();
+		Random rand = new Random();
+
+        try(BufferedReader br = new BufferedReader(new FileReader("puzzles.txt"))) {
+    		for(String line; (line = br.readLine()) != null; ) {
+        		puzzleList.add(line);
+		    }
+		} catch(IOException e) {
+			System.err.println("Caught IOException: " + e.getMessage());
+		}
+
+		String output = puzzleList.get(rand.nextInt(puzzleList.size() - 1) + 1);
+		return output;
 	}
 	
 	public static void createPuzzles(int puzzleNum){
+		if(puzzleNum == 1){
+			System.out.println("Creating those super hard puzzles...");
+		}
+		if(puzzleNum == 1024){
+			System.out.println("Finally done!");
+		}
 		//Creates byte arry (16 byte) of all 0's for the start of the puzzle
 		byte[] puzzleStart = new byte[16];
 		
@@ -43,60 +134,39 @@ public class Test {
 		bytedKey = puzzleKey.getEncoded();
 		
 		//concatenates 2 (or more) byte arrays 
-		byte[] c = new byte[26];
+		byte[] puzzle = new byte[26];
 		try {
 			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 			outputStream.write(puzzleStart); //16 byte
 			outputStream.write(uniqueNum); //2 byte
 			outputStream.write(bytedKey); //8 byte
-			c = outputStream.toByteArray();
-			
-			//System.out.println(c);
-			//System.out.println(c.length);
-			//System.out.println(byteArrayToString(c));
-			/*
-			keep this for now, will be useful later
-			System.out.println("concatarray = " + byteArrayToString(c));
-			//These return the original inputs by splitting the byte array 
-			byte[] arr2 = Arrays.copyOfRange(c, 18, 26);
-	   		System.out.println("DESkey (after copy) = " + byteArrayToString(arr2));
+			puzzle = outputStream.toByteArray();
+		} catch (Exception e) { System.err.println("Caught Exception: " + e.getMessage()); }
 
-			byte[] arr3 = Arrays.copyOfRange(c, 0, 15);
-	   		System.out.println("puzzlestart (after copy) = " + byteArrayToString(arr3));*/
-	
-		} catch (Exception e) {
-			  System.err.println("Caught Exception: " + e.getMessage());
-		}
-		
 		//Create the byte array to be turned into the key to encrypt the puzzle
-		
 		//2 bytes of random data 
-		byte[] placeholder0 = new byte[2];
-		new SecureRandom().nextBytes(placeholder0);
+		byte[] keyStart = new byte[2];
+		new SecureRandom().nextBytes(keyStart);
 		
 		//6 bytes of 0's
-		byte[] placeholder1 = new byte[6];
+		byte[] keyEnd = new byte[6];
 		
 		//Concat them together
-		byte[] anotherPlaceholder = new byte[8];
+		byte[] fullKey = new byte[8];
 		try {
 			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-			outputStream.write(placeholder0);
-			outputStream.write(placeholder1);
-			anotherPlaceholder = outputStream.toByteArray();
-		} catch (Exception e) {
-			  System.err.println("Caught Exception: " + e.getMessage());
-		}
+			outputStream.write(keyStart);
+			outputStream.write(keyEnd);
+			fullKey = outputStream.toByteArray();
+		} catch (Exception e) { System.err.println("Caught Exception: " + e.getMessage()); }
 		
 		//Creates DESKey to encrypt the puzzle from the above byte array 
-		SecretKey puzzleEncryptKey = generateDESKey(anotherPlaceholder);
+		SecretKey puzzleEncryptKey = generateDESKey(fullKey);
 		
 		String encryptedText = "";
 		try {
-			encryptedText = encrypt(c, puzzleEncryptKey);
-		} catch (Exception e){
-			System.err.println("Caught Exception: " + e.getMessage());
-		}
+			encryptedText = encrypt(puzzle, puzzleEncryptKey);
+		} catch (Exception e){ System.err.println("Caught Exception: " + e.getMessage()); }
 		
 		try {
 			PrintWriter out = new PrintWriter(new FileWriter("puzzles.txt", true));
@@ -107,8 +177,9 @@ public class Test {
 		}
 	}
 		
-	
 	//Creates DESKey 
+	//Had to put it in it's own method as calling the createKey method required 
+	//the 3 catch blocks and were taking up lots of space
 	public static SecretKey generateDESKey(byte[] key){
 		SecretKey DESKey = null;
 		try {
@@ -119,12 +190,11 @@ public class Test {
 			  System.err.println("Caught InvalidKeySpecException: " + e.getMessage());
 		} catch (InvalidKeyException e) {
 			  System.err.println("Caught InvalidKeyException: " + e.getMessage());
-		}
-		
+		}		
 		return DESKey;
 	}
 	
-	//Note: This method is not my own and is taken from the Lab 3 AES work (with a few changes of course)
+	//Note: This method is not my own and is taken from the Lab 3 AES work (with a few changes)
 	public static String encrypt(byte[] plainByte, SecretKey secretKey) throws Exception {
 		cipher = Cipher.getInstance("DES");
 		//Initialise the cipher to be in encrypt mode, using the given key.
@@ -139,7 +209,6 @@ public class Test {
 		//System.out.println(encryptedText);
 		return encryptedText;
 	}
-
 
 /**
  * A collection of useful methods for CSF207's Coursework 2.
@@ -216,5 +285,6 @@ public class Test {
 		return decoder.decode(s);
 	}
 }
+
 
 
